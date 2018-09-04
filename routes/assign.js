6beb310d079f043d98ea196a2b4339d689c39087
zakir shaikh <zakrshkh07@gmail.com>
2018-09-04 20:16:19 +0530

    assign edit complete

--- routes/assign.js
+++ routes/assign.js
@@ -103,8 +103,8 @@ router.post('/add/:campaignId', oauth.authorise(), (req, res, next) => {
     }
     client.query('BEGIN;');
         employee.forEach(function(val,key){
-          var singleInsert = "INSERT INTO campaign_employee_master(cem_cm_id,cem_emp_id) VALUES ($1,$2) RETURNING *",
-        params = [id,val.emp_id]
+          var singleInsert = "INSERT INTO campaign_employee_master(cem_cm_id,cem_emp_id,cem_select) VALUES ($1,$2,$3) RETURNING *",
+        params = [id,val.emp_id,val.select]
        
         client.query(singleInsert, params, function (error, result) {
         results.push(result.rows[0]);// Will contain your inserted rows
@@ -119,9 +119,51 @@ router.post('/add/:campaignId', oauth.authorise(), (req, res, next) => {
   });
 });
 
-router.get('/edit', oauth.authorise(), (req, res, next) => {
+
+
+router.post('/edit', oauth.authorise(), (req, res, next) => {
   const results = [];
-  const id=req.params.dmId;
+  const id = req.params.campaignId;
+  const employee=req.body.employee;
+  const remove=req.body.remove;
+  console.log(remove);
+  console.log(employee)
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, contact: err});
+    }
+    client.query('BEGIN;');
+    employee.forEach(function(val,key){
+    var singleInsert = 'update campaign_employee_master set cem_cm_id=$1, cem_emp_id=$2, cem_updated_at=now()  where cem_id=$3 RETURNING *',
+        params = [val.cem_cm_id,val.emp_id,val.cem_id];
+        console.log(params);
+    client.query(singleInsert, params, function (error, result) {
+      console.log(result);
+        results.push(result.rows[0]); // Will contain your inserted rows
+        
+     
+       
+        
+      });
+
+    });
+
+    remove.forEach(function(val,index){
+           client.query("delete from campaign_employee_master where cem_id=$1",[val.cem_id]);
+        });
+    client.query('COMMIT;');
+        done();
+        return res.json(results);
+    done(err);
+  });
+});
+
+router.get('/campemp/:assignId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id=req.params.assignId;
   pool.connect(function(err, client, done){
     if(err) {
       done();
@@ -129,7 +171,7 @@ router.get('/edit', oauth.authorise(), (req, res, next) => {
       console.log("the error is"+err);
       return res.status(500).json({success: false, data: err});
     }
-    const query = client.query("SELECT * FROM campaign_employee_master cem left outer join campaign_master cm on cem.cem_cm_id=cm.cm_id");
+    const query = client.query("SELECT * FROM employee_master emp left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id where cem_cm_id=$1",[id]);
     query.on('row', (row) => {
       results.push(row);
 
@@ -156,7 +198,8 @@ router.post('/employee/total', oauth.authorise(), (req, res, next) => {
 
     console.log(str);
     const strqry =  "SELECT count(emp_id) as total "+
-                    "from employee_master "+
+                    "from employee_master emp "+
+                    "left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id "+
                     "where emp_status = 0 "+
                     "and LOWER(emp_name||''||emp_mobile||''||emp_designation) LIKE LOWER($1);";
 
@@ -187,6 +230,7 @@ router.post('/employee/limit', oauth.authorise(), (req, res, next) => {
 
     const strqry =  "SELECT * "+
                     "FROM employee_master emp "+
+                    "left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id "+
                     "where emp.emp_status = 0 "+
                     "and LOWER(emp_name||''||emp_mobile||''||emp_designation) LIKE LOWER($1) "+
                     "order by emp.emp_id desc LIMIT $2 OFFSET $3";
