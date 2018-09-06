d9e07d10c3196da1c8e03f017edd9cde34d82542
zakir shaikh <zakrshkh07@gmail.com>
2018-09-06 18:25:41 +0530

    assign job and telecaller changes

--- routes/assign.js
+++ routes/assign.js
@@ -4,12 +4,13 @@ var oauth = require('../oauth/index');
 var pg = require('pg');
 var path = require('path');
 var config = require('../config.js');
+var encryption = require('../commons/encryption.js');
 
 var pool = new pg.Pool(config);
 
-router.get('/emp/view', oauth.authorise(), (req, res, next) => {
+router.get('/emp/view/:campaignId', oauth.authorise(), (req, res, next) => {
   const results = [];
-  const id=req.params.dmId;
+  const id=req.params.campaignId;
   pool.connect(function(err, client, done){
     if(err) {
       done();
@@ -17,7 +18,7 @@ router.get('/emp/view', oauth.authorise(), (req, res, next) => {
       console.log("the error is"+err);
       return res.status(500).json({success: false, data: err});
     }
-    const query = client.query("SELECT emp_id,emp_name,emp_mobile FROM employee_master where emp_status=0");
+    const query = client.query("SELECT * FROM employee_master emp left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id where emp_status=0 and cem_cm_id=$1",[id]);
     query.on('row', (row) => {
       results.push(row);
 
@@ -74,8 +75,8 @@ router.post('/assign/:jobId', oauth.authorise(), (req, res, next) => {
           client.query(singleInsert, params, function (error, result) {
           results.push(result.rows[0]);// Will contain your inserted rows
           
-          client.query('update contact_discovery_master set cdm_status=1, cdm_updated_at=now() where cdm_id=$1 RETURNING *',[val.cdm_id])
-        
+          /*client.query('update contact_discovery_master set cdm_status=1, cdm_updated_at=now() where cdm_id=$1 RETURNING *',[val.cdm_id])
+        */
       });
           client.query('COMMIT;');
         done();
@@ -88,46 +89,12 @@ router.post('/assign/:jobId', oauth.authorise(), (req, res, next) => {
   });
 });
 
-
-
-router.post('/add/:campaignId', oauth.authorise(), (req, res, next) => {
-  const results = [];
-  const id = req.params.campaignId;
-  const employee=req.body;
-  pool.connect(function(err, client, done){
-    if(err) {
-      done();
-      // pg.end();
-      console.log("the error is"+err);
-      return res.status(500).json({success: false, contact: err});
-    }
-    client.query('BEGIN;');
-        employee.forEach(function(val,key){
-          var singleInsert = "INSERT INTO campaign_employee_master(cem_cm_id,cem_emp_id,cem_select) VALUES ($1,$2,$3) RETURNING *",
-        params = [id,val.emp_id,val.select]
-       
-        client.query(singleInsert, params, function (error, result) {
-        results.push(result.rows[0]);// Will contain your inserted rows
-        
-    });
-        client.query('COMMIT;');
-        done();
-        return res.json(results);
-  });
-        
-    done(err);
-  });
-});
-
-
-
-router.post('/edit', oauth.authorise(), (req, res, next) => {
+router.post('/edit/:campaignId', oauth.authorise(), (req, res, next) => {
   const results = [];
   const id = req.params.campaignId;
   const employee=req.body.employee;
   const remove=req.body.remove;
-  console.log(remove);
-  console.log(employee)
+  const newemp=req.body.newemp;
   pool.connect(function(err, client, done){
     if(err) {
       done();
@@ -137,22 +104,19 @@ router.post('/edit', oauth.authorise(), (req, res, next) => {
     }
     client.query('BEGIN;');
     employee.forEach(function(val,key){
-    var singleInsert = 'update campaign_employee_master set cem_cm_id=$1, cem_emp_id=$2, cem_updated_at=now()  where cem_id=$3 RETURNING *',
+    var singleInsert = 'update campaign_employee_master set cem_cm_id=$1, cem_emp_id=$2 where cem_id=$3 RETURNING *',
         params = [val.cem_cm_id,val.emp_id,val.cem_id];
         console.log(params);
     client.query(singleInsert, params, function (error, result) {
-      console.log(result);
+      
         results.push(result.rows[0]); // Will contain your inserted rows
-        
-     
-       
-        
       });
-
     });
-
-    remove.forEach(function(val,index){
-           client.query("delete from campaign_employee_master where cem_id=$1",[val.cem_id]);
+        remove.forEach(function(val,index){
+          client.query("delete from campaign_employee_master where cem_id=$1",[val.cem_id]);
+        });
+        newemp.forEach(function(value,key){
+          client.query('INSERT INTO campaign_employee_master(cem_cm_id,cem_emp_id) VALUES ($1,$2)',[id,value.emp_id]);
         });
     client.query('COMMIT;');
         done();
@@ -200,6 +164,7 @@ router.post('/employee/total', oauth.authorise(), (req, res, next) => {
     const strqry =  "SELECT count(emp_id) as total "+
                     "from employee_master emp "+
                     "left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id "+
+                    "left outer join campaign_master cm on cem.cem_cm_id=cm.cm_id "+
                     "where emp_status = 0 "+
                     "and LOWER(emp_name||''||emp_mobile||''||emp_designation) LIKE LOWER($1);";
 
@@ -231,12 +196,93 @@ router.post('/employee/limit', oauth.authorise(), (req, res, next) => {
     const strqry =  "SELECT * "+
                     "FROM employee_master emp "+
                     "left outer join campaign_employee_master cem on cem.cem_emp_id=emp.emp_id "+
+                    "left outer join campaign_master cm on cem.cem_cm_id=cm.cm_id "+
                     "where emp.emp_status = 0 "+
                     "and LOWER(emp_name||''||emp_mobile||''||emp_designation) LIKE LOWER($1) "+
                     "order by emp.emp_id desc LIMIT $2 OFFSET $3";
 
     const query = client.query(strqry,[ str, req.body.number, req.body.begin]);
     query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+    done(err);
+  });
+});
+
+router.post('/contact/total', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, contact: err});
+    }
+    const str = "%"+req.body.search+"%";
+
+    console.log(str);
+    const strqry =  "SELECT count(cdm_id) as total "+
+                    "from contact_discovery_master "+
+                    "where cdm_status=0 "+
+                    "and LOWER(cdm_first_name||''||cdm_last_name) LIKE LOWER($1);";
+
+    const query = client.query(strqry,[str]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+    done(err);
+  });
+});
+
+router.post('/contact/limit', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, contact: err});
+    }
+    const str = "%"+req.body.search+"%";
+    // SQL Query > Select contact
+
+    const strqry =  "SELECT * "+
+                    "FROM contact_discovery_master cdm "+
+                    "left outer join campaign_master cm on cdm.cdm_cm_id=cm.cm_id "+
+                    "where cdm.cdm_status = 0 "+
+                    "and LOWER(cm_campaign_name||''||cdm_first_name||''||cdm_last_name) LIKE LOWER($1) "+
+                    "order by cdm.cdm_id desc LIMIT $2 OFFSET $3";
+
+    const query = client.query(strqry,[ str, req.body.number, req.body.begin]);
+    query.on('row', (row) => {
+      row.cdm_mobile=encryption.decrypt(row.cdm_mobile);
+      row.cdm_first_name=encryption.decrypt(row.cdm_first_name);
+      row.cdm_last_name=encryption.decrypt(row.cdm_last_name);
+      row.cdm_job_title=encryption.decrypt(row.cdm_job_title);
+      row.cdm_job_level=encryption.decrypt(row.cdm_job_level);
+      row.cdm_dept=encryption.decrypt(row.cdm_dept);
+      row.cdm_email_id=encryption.decrypt(row.cdm_email_id);
+      row.cdm_company_name=encryption.decrypt(row.cdm_company_name);
+      row.cdm_address=encryption.decrypt(row.cdm_address);
+      row.cdm_city=encryption.decrypt(row.cdm_city);
+      row.cdm_state=encryption.decrypt(row.cdm_state);
+      row.cdm_postal_code=encryption.decrypt(row.cdm_postal_code);
+      row.cdm_country=encryption.decrypt(row.cdm_country);
+      row.cdm_industry=encryption.decrypt(row.cdm_industry);
+      row.cdm_company_size=encryption.decrypt(row.cdm_company_size);
+      row.cdm_revenue=encryption.decrypt(row.cdm_revenue);
+      row.cdm_asset=encryption.decrypt(row.cdm_asset);
+      row.cdm_domain=encryption.decrypt(row.cdm_domain);
       results.push(row);
     });
     query.on('end', () => {
--- routes/telecaller.js
+++ routes/telecaller.js
@@ -17,7 +17,7 @@ router.get('/', oauth.authorise(), (req, res, next) => {
       console.log("the error is"+err);
       return res.status(500).json({success: false, contact: err});
     }
-    const query = client.query("SELECT * FROM contact_discovery_master cdm left outer join campaign_master cm on cdm.cdm_cm_id=cm.cm_id where cdm_status=0");
+    const query = client.query("SELECT * FROM contact_discovery_master cdm left outer join campaign_master cm on cdm.cdm_cm_id=cm.cm_id where cdm_status=0 and call_status='open' ORDER BY cdm_id ASC LIMIT 1");
     query.on('row', (row) => {
       row.cdm_mobile=encryption.decrypt(row.cdm_mobile);
       row.cdm_first_name=encryption.decrypt(row.cdm_first_name);
@@ -37,6 +37,83 @@ router.get('/', oauth.authorise(), (req, res, next) => {
       row.cdm_revenue=encryption.decrypt(row.cdm_revenue);
       row.cdm_asset=encryption.decrypt(row.cdm_asset);
       row.cdm_domain=encryption.decrypt(row.cdm_domain);
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+  done(err);
+  });
+});
+
+router.post('/status/:jobId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.jobId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, contact: err});
+    }
+    client.query('BEGIN;');
+
+    var singleInsert = "update contact_discovery_master set call_status=$1, cdm_updated_at=now() where cdm_id=$2 RETURNING *",
+        params = [req.body.call_status,id]
+    client.query(singleInsert, params, function (error, result) {
+        results.push(result.rows[0]); // Will contain your inserted rows
+        done();
+        client.query('COMMIT;');
+        return res.json(results);
+    });
+
+    done(err);
+  });
+});
+
+router.post('/add', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const contact = req.body.contact;
+  const follow = req.body.follow;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    client.query('BEGIN;');
+    var singleInsert = "INSERT INTO followup_master(fm_date,fm_comment) values($1,$2) RETURNING *",
+        params = [follow.fm_date,follow.fm_comment]
+    client.query(singleInsert, params, function (error, result) {
+        results.push(result.rows[0]); // Will contain your inserted rows
+
+        contact.forEach(function(val,key){
+          client.query('update followup_master set fm_cdm_id=$1 where fm_id=$2',[val.cdm_id,result.rows[0].fm_id]);
+        });
+        client.query('COMMIT;');
+        done();
+        return res.json(results);
+    });
+
+    done(err);
+  });
+});
+
+router.get('/:jobId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.jobId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, contact: err});
+    }
+    const query = client.query("SELECT fm_date,fm_comment,fm_cdm_id FROM followup_master where fm_cdm_id=$1",[id]);
+    query.on('row', (row) => {
       results.push(row);
     });
     query.on('end', () => {
