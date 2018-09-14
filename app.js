202a95835a9e9fb99b22832e0632e1e824f818fb
zakir shaikh <zakrshkh07@gmail.com>
2018-09-14 18:32:51 +0530

    changes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var roles = require("user-groups-roles");
 // var nodemailer = require('nodemailer');
 
 var oauth = require('./oauth');
